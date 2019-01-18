d60a520c52b7f78a7c2ebd5dbb91a7e1e91d92d9
Ryuis <ryuis1124@gmail.com>
2019-01-18 18:05:30 +0900

    Dynamic response practice

--- nodejs/express basic/app.js
+++ nodejs/express basic/app.js
@@ -4,7 +4,72 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/",(req, res) => {
-    res.send("Home Page");
+    var text = `<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>sketch</title>
+    <style>
+        header {
+            position: absolute;
+            background: blue;
+            top: 0px;
+            width: 100%;
+            height: 100px;
+        }
+
+        footer {
+            position: absolute;
+            background: yellow;
+            bottom: 0px;
+            width: 100%;
+            height: 100px;
+        }
+
+        a:link {
+            color: red;
+        }
+
+        a:visited {
+            color: pink;
+        }
+
+        a {
+            color: black;
+        }
+
+        a:hover {
+            font-size: 50px;
+            color: green;
+        }
+    </style>
+</head>
+
+<body>
+    <script>
+        var obj = { 'name' : 1};
+
+
+    </script>
+    <header>
+        <h1>This is sketch</h1>
+
+    </header>
+    <main>
+        <h3>list of resourceful sites </h3>
+        <ul>
+            <li><a href="https://github.com/Ryuis92">Github</a></li>
+        </ul>
+    </main>
+    <footer>
+
+        <h1>Here is footer</h1>
+    </footer>
+</body>
+
+</html>`
+
+    res.send(text);
 });
 
 app.get("/login",(req, res) => {
