f32c8e802691b78c1691bed9540c3140b50a664b
Ryuis <ryuis1124@gmail.com>
2019-01-20 19:42:56 +0900

    Post method and body-parser

--- nodejs/express basic/app.js
+++ nodejs/express basic/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
 
 app.set("view engine" , "jade");
 app.set("views", "./views");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/topic/:id", (req, res) =>{
-  res.send(req.params);
+  res.send(req.param);
 });
-
 app.get("/",(req, res) => {
     res.render("first", {time: Date()});
 });
