8f4c52f37624f3091341218fb594741f0b10c989
Ryuis <ryuis1124@gmail.com>
2019-01-21 20:34:14 +0900

    Simple web application with Node.js Express jade

---
+++ nodejs/Simple web application/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const app = express();
+
+app.locals.pretty = true;
+
+app.set("view engine", "jade");
+app.set("views", "./views");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.post("/topic", (req, res) => {
+    var title = req.body.title;
+    var content = req.body.description;
+    fs.writeFile(`./data/${title}`, content, "utf-8", (err) => {
+        if (err) {
+            console.log(e);
+            res.status(500).send("Internal server error");
+        }
+    res.redirect("/topic/" + title);
+        
+    });
+});
+
+
+app.get("/topic/new", (req, res) => {
+    fs.readdir("./data", (err, files) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal server error");
+        }
+        res.render("new", {files: files, topics: files});
+    });
+});
+
+app.get(["/topic", "/topic/:topic"], (req, res) => {
+    fs.readdir("./data", (err, files) => {
+        var topic = req.params.topic;
+        if (topic) {
+            fs.readFile("./data/" + topic, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send("Internal server error");
+                }
+                res.render("topic", {files: files, title: topic, content: data});
+            })
+
+        } else {
+            res.render("topic", {files: files, title: "Hello", content: "Good to see you"});
+        }
+    })
+});
+
+app.listen(8888, () => {
+    console.log("Server Connected 8888 port!");
+});
